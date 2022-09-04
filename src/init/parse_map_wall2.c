898ea3e66c1d12bfb45fadcb9ef2495535f163d7
lschrafs <lschrafs@student.42wolfsburg.de>
2022-09-04 19:19:00 +0200

    Refctoring

--- src/init/parse_map_wall2.c
+++ src/init/parse_map_wall2.c
@@ -6,17 +6,25 @@
 /*   By: lschrafs <lschrafs@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/31 10:55:46 by lschrafs          #+#    #+#             */
-/*   Updated: 2022/09/02 09:57:01 by lschrafs         ###   ########.fr       */
+/*   Updated: 2022/09/04 19:18:29 by lschrafs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3d.h"
 #include "../utils/hashtable.h"
 
-static void	close_free_exit(char **str_arr, t_data *data, int fd)
+static void	close_free_exit_1(char **str_arr, t_data *data, int fd)
 {
 	close(fd);
 	free_strarray(str_arr);
+	error_msg_exit(data, "Error parsing wall image properties");
+}
+
+static void	close_free_exit_2(t_data *data, char *line, char *helper, int fd)
+{
+	close(fd);
+	free(line);
+	free(helper);
 	error_msg_exit(data, "Error parsing wall image properties");
 }
 
@@ -31,12 +39,12 @@ void	wall_parse_properties(t_image *wall, char *line, t_data *data, int fd)
 	str_arr = ft_split(helper, ' ');
 	free(helper);
 	if (str_arr_len(str_arr) != 4)
-		close_free_exit(str_arr, data, fd);
+		close_free_exit_1(str_arr, data, fd);
 	i = 0;
 	while (str_arr[i])
 	{
 		if (!is_number(str_arr[i]))
-			close_free_exit(str_arr, data, fd);
+			close_free_exit_1(str_arr, data, fd);
 		i++;
 	}
 	e = 0;
@@ -45,7 +53,7 @@ void	wall_parse_properties(t_image *wall, char *line, t_data *data, int fd)
 	wall->n_keys = ft_atoi_e(str_arr[2], &e);
 	wall->key_len = ft_atoi_e(str_arr[3], &e);
 	if (e == 1)
-		close_free_exit(str_arr, data, fd);
+		close_free_exit_1(str_arr, data, fd);
 	free_strarray(str_arr);
 }
 
@@ -58,8 +66,8 @@ void	wall_parse_keys(t_image *wall, char *line, t_data *data, int fd)
 	helper1 = ft_strtrim(line, ",\n");
 	helper2 = ft_strtrim(helper1, "\"");
 	free(helper1);
-	if ((int)ft_strlen(helper2) != wall->key_len + 10 ||
-		ft_strncmp(&(helper2[wall->key_len]), " c #", 4) ||
+	if ((int)ft_strlen(helper2) != wall->key_len + 10 || \
+		ft_strncmp(&(helper2[wall->key_len]), " c #", 4) || \
 		!is_hex(&(helper2[wall->key_len + 4])))
 	{
 		free(helper2);
@@ -68,9 +76,8 @@ void	wall_parse_keys(t_image *wall, char *line, t_data *data, int fd)
 	}
 	key = ft_calloc(wall->key_len + 1, 1);
 	ft_strlcat(key, helper2, wall->key_len + 1);
-	hashtable_add(wall->hashtable, 100, key, ft_atoi_base_e(&(helper2[wall->key_len + 4]), 16, 0));
-	// printf("Line: (%s) is (%i) in decimal\n", &(helper2[wall->key_len + 4]), ft_atoi_base_e(&(helper2[wall->key_len + 4]), 16, 0));
-	// printf("Found key (%s); value: (%i)\n", key, hashtable_get(wall->hashtable, 100, key));
+	hashtable_add(wall->hashtable, 100, key, \
+					ft_atoi_base_e(&(helper2[wall->key_len + 4]), 16, 0));
 	free(key);
 	free(helper2);
 }
@@ -85,25 +92,16 @@ void	wall_parse_pixels(t_image *wall, char *line, int fd, int i)
 	helper2 = ft_strtrim(helper1, "\"");
 	free(helper1);
 	if ((int)ft_strlen(helper2) != wall->width * wall->key_len)
-	{
-		printf("Line %i, line_len: %i\n", i, (int)ft_strlen(helper2));
-		printf("Wall width: %i, key len: %i\n", wall->width, wall->key_len);
-		free(line);
-		close(fd);
-		error_msg_exit(wall->data, "Error reading wall image pixels");
-	}
+		close_free_exit_2(wall->data, line, helper2, fd);
 	j = 0;
 	while (j < wall->width)
 	{
-		helper1 = get_first_n_chars(&(helper2[j * wall->key_len]), wall->key_len);
+		helper1 = get_first_n_chars(&(helper2[j * wall->key_len]), \
+									wall->key_len);
 		if (!helper1 || (i - 6 - wall->n_keys < 0))
-		{
-			free(helper2);
-			free(line);
-			close(fd);
-			error_msg_exit(wall->data, "Error mallocing while parsing pixels");
-		}
-		wall->pixels[i - 6 - wall->n_keys][j] = hashtable_get(wall->hashtable, 100, helper1);
+			close_free_exit_2(wall->data, line, helper2, fd);
+		wall->pixels[i - 6 - wall->n_keys][j] = \
+								hashtable_get(wall->hashtable, 100, helper1);
 		free(helper1);
 		j++;
 	}
